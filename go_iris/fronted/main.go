7fe80a9ac41745f4d53673a39be7b8d1ce0ab6f1
caoting <307298900@qq.com>
2020-10-13 23:58:48 +0800

    main

---
+++ go_iris/fronted/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/kataras/iris/v12"
+)
+
+func main() {
+	// 1. 创建iris实例
+	app := iris.New()
+
+	// 2. 设置错误模式，在mvc模式下提示错误
+	app.Logger().SetLevel("debug")
+
+	// 3. 注册模板
+	temp := iris.HTML("./fronted/web/views",
+		".html").Layout(
+		"shared/layout.html").Reload(true)
+	app.RegisterView(temp)
+
+	// 4.  设置模版
+	// 旧版本的方法： app.StaticWeb("/assets", "./fronted/web/assets")
+	app.HandleDir("/assets", iris.Dir("./fronted/web/assets"))
+
+	//访问生成好的html静态文件
+	app.HandleDir("/html", iris.Dir("./fronted/web/htmlProductShow"))
+
+	//出现异常跳转到指定页面
+	app.OnAnyErrorCode(func(ctx iris.Context) {
+		ctx.ViewData("message", ctx.Values().GetStringDefault("message", "访问页面出错"))
+		ctx.ViewLayout("")
+		ctx.View("shared/error/html")
+	})
+
+	// 6. 连接数据库
+	//db, err := common.NewMysqlConn()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//sess := sessions.New(sessions.Config{
+	//	Cookie:  "AdminCookie",
+	//	Expires: 600 * time.Minute,
+	//})
+	//
+	//ctx, cancel := context.WithCancel(context.Background())
+	//defer cancel()
+
+	// 注册控制器
+
+	app.Run(
+		iris.Addr(":12998"),
+		iris.WithoutServerError(iris.ErrServerClosed),
+		iris.WithOptimizations,
+	)
+
+}
