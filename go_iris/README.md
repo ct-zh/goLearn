1bf0a9a66dd6cd9aa305142ba0d86c619acecf67
caoting <307298900@qq.com>
2020-10-16 16:06:33 +0800

    api test

--- go_iris/README.md
+++ go_iris/README.md
@@ -9,6 +9,13 @@
 
 ## todo
 1. repository的newInstance逻辑，db参数需要支持多种数据库
-2. common的代码 
+2. common的代码
+    ```
+    // 控制器参数
+    dec := common.NewDecoder(&common.DecoderOptions{TagName: "imooc"})
+    if err := dec.Decode(p.Ctx.Request().Form, product); err != nil {
+       //  
+    }
+    ```
 3. 后台接口用API格式，前端商品页面展示
 
--- go_iris/backend/web/controllers/product.go
+++ go_iris/backend/web/controllers/product.go
@@ -2,10 +2,9 @@ package controllers
 
 import (
 	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/mvc"
-	"go_iris/common"
 	"go_iris/datamodels"
 	"go_iris/services"
+	"go_iris/tool"
 	"strconv"
 )
 
@@ -14,85 +13,180 @@ type ProductController struct {
 	Service services.IProductService
 }
 
-func (p *ProductController) GetAll() mvc.View {
+func (p *ProductController) GetAll() interface{} {
 	products, _ := p.Service.GetAll()
-	return mvc.View{
-		Name: "product/view.html",
-		Data: iris.Map{
-			"productArray": products,
-		},
+	return tool.Output{
+		Code: 200,
+		Msg:  "",
+		Data: products,
 	}
 }
 
-func (p *ProductController) PostUpdate() {
-	product := &datamodels.Product{}
+func (p *ProductController) PostUpdate() interface{} {
+	id, err := strconv.ParseInt(p.Ctx.PostValue("id"), 10, 64)
+	if err != nil {
+		return tool.Output{
+			Code: -1,
+			Msg:  "id非法",
+		}
+	}
 
-	p.parseForm(product)
+	product, err := p.Service.GetById(id)
+	if err != nil {
+		return tool.Output{
+			Code: -1,
+			Msg:  err.Error(),
+		}
+	}
 
-	err := p.Service.Update(product)
+	var (
+		productName  = p.Ctx.PostValue("productName")
+		productImage = p.Ctx.PostValue("productImage")
+		productUrl   = p.Ctx.PostValue("productUrl")
+	)
+
+	flag := false
+
+	productNum, err := strconv.ParseInt(p.Ctx.PostValue("productNum"), 10, 64)
+	if err == nil && productNum > 0 {
+		product.ProductNum = productNum
+		flag = true
+	}
+	if len(productName) > 0 {
+		product.ProductName = productName
+		flag = true
+	}
+	if len(productImage) > 0 {
+		product.ProductImage = productImage
+		flag = true
+	}
+	if len(productUrl) > 0 {
+		product.ProductUrl = productUrl
+		flag = true
+	}
+
+	if !flag {
+		return tool.Output{
+			Code: 0,
+			Msg:  "没有需要更新的数据",
+			Data: product,
+		}
+	}
+
+	if err := p.Service.Update(product); err != nil {
+		return tool.Output{
+			Code: -1,
+			Msg:  err.Error(),
+		}
+	}
+	return tool.Output{
+		Code: 0,
+		Msg:  "success",
+		Data: product,
+	}
+}
+
+func (p *ProductController) PostAdd() interface{} {
+	var (
+		productName  = p.Ctx.PostValue("productName")
+		productImage = p.Ctx.PostValue("productImage")
+		productUrl   = p.Ctx.PostValue("productUrl")
+	)
+	productNum, err := strconv.ParseInt(p.Ctx.PostValue("productNum"), 10, 64)
+	if err != nil || productNum <= 0 {
+		return tool.Output{
+			Code: -1,
+			Msg:  "商品库存非法",
+		}
+	}
+
+	if len(productName) == 0 {
+		return tool.Output{
+			Code: -1,
+			Msg:  "商品标题不能为空",
+		}
+	}
+
+	if len(productImage) == 0 {
+		return tool.Output{
+			Code: -1,
+			Msg:  "商品图片不能为空",
+		}
+	}
+
+	if len(productUrl) == 0 {
+		return tool.Output{
+			Code: -1,
+			Msg:  "商品地址不能为空",
+		}
+	}
+
+	product := datamodels.Product{
+		ProductName:  productName,
+		ProductNum:   productNum,
+		ProductImage: productImage,
+		ProductUrl:   productUrl,
+	}
+
+	_, err = p.Service.Insert(&product)
 	if err != nil {
 		p.Ctx.Application().Logger().Debug(err)
+		return tool.Output{
+			Code: -1,
+			Msg:  err.Error(),
+		}
 	}
-	p.Ctx.Redirect("/product/all")
+
+	return tool.Output{
+		Code: 0,
+		Msg:  "success",
+		Data: product,
+	}
 }
 
-func (p *ProductController) GetAdd() mvc.View {
-	return mvc.View{
-		Name: "product/add.html",
-	}
-}
-
-func (p *ProductController) PostAdd() {
-	product := &datamodels.Product{}
-
-	p.parseForm(product)
-
-	_, err := p.Service.Insert(product)
-	if err != nil {
-		p.Ctx.Application().Logger().Debug(err)
-	}
-	p.Ctx.Redirect("/product/all")
-}
-
-func (p *ProductController) GetManager() mvc.View {
+func (p *ProductController) GetOne() interface{} {
 	idString := p.Ctx.URLParam("id")
 	id, err := strconv.ParseInt(idString, 10, 16)
 	if err != nil {
 		p.Ctx.Application().Logger().Debug(err)
+		return tool.Output{
+			Code: -1,
+			Msg:  err.Error(),
+		}
 	}
 
 	product, err := p.Service.GetById(id)
 	if err != nil {
 		p.Ctx.Application().Logger().Debug(err)
+		return tool.Output{
+			Code: -1,
+			Msg:  err.Error(),
+		}
 	}
 
-	return mvc.View{
-		Name: "product/manager.html",
-		Data: iris.Map{
-			"product": product,
-		},
+	return tool.Output{
+		Code: 0,
+		Msg:  "success",
+		Data: product,
 	}
 }
 
-func (p *ProductController) GetDelete() {
+func (p *ProductController) GetDelete() interface{} {
 	idString := p.Ctx.URLParam("id")
 	id, err := strconv.ParseInt(idString, 10, 16)
 	if err != nil {
 		p.Ctx.Application().Logger().Debug(err)
 	}
 
-	if ok := p.Service.DeleteById(id); ok {
-		p.Ctx.Application().Logger().Debug("删除商品成功，ID为：" + idString)
+	if p.Service.DeleteById(id) {
+		return tool.Output{
+			Code: -1,
+			Msg:  "删除商品成功，ID为：" + idString,
+		}
 	} else {
-		p.Ctx.Application().Logger().Debug("删除商品失败，ID为：" + idString)
-	}
-	p.Ctx.Redirect("/product/all")
-}
-
-func (p *ProductController) parseForm(product *datamodels.Product) {
-	p.Ctx.Request().ParseForm()
-	dec := common.NewDecoder(&common.DecoderOptions{TagName: "imooc"})
-	if err := dec.Decode(p.Ctx.Request().Form, product); err != nil {
-		p.Ctx.Application().Logger().Debug(err)
+		return tool.Output{
+			Code: -1,
+			Msg:  "删除商品失败，商品可能已被删除",
+		}
 	}
 }
