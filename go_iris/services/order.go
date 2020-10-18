5f83d7aa09091bda57363a48765909d386c03623
caoting <307298900@qq.com>
2020-10-19 00:02:11 +0800

    order services

--- go_iris/services/order.go
+++ go_iris/services/order.go
@@ -1,12 +1,45 @@
 package services
 
-import "go_iris/repositories"
+import (
+	"go_iris/datamodels"
+	"go_iris/repositories"
+)
 
 type IOrderService interface {
+	GetById(int64) (*datamodels.Order, error)
+	DeleteById(int64) bool
+	Update(order *datamodels.Order) error
+	Insert(order *datamodels.Order) (int64, error)
+	GetAll() ([]*datamodels.Order, error)
+	GetAllWithInfo() (map[int]map[string]string, error)
 }
 
 type OrderService struct {
 	repository repositories.IOrder
+}
+
+func (o OrderService) GetById(id int64) (*datamodels.Order, error) {
+	return o.repository.SelectByKey(id)
+}
+
+func (o OrderService) DeleteById(id int64) bool {
+	return o.repository.Delete(id)
+}
+
+func (o OrderService) Update(order *datamodels.Order) error {
+	return o.repository.Update(order)
+}
+
+func (o OrderService) Insert(order *datamodels.Order) (int64, error) {
+	return o.repository.Insert(order)
+}
+
+func (o OrderService) GetAll() ([]*datamodels.Order, error) {
+	return o.repository.SelectAll()
+}
+
+func (o OrderService) GetAllWithInfo() (map[int]map[string]string, error) {
+	return o.repository.SelectAllWithInfo()
 }
 
 func NewService(r repositories.IOrder) IOrderService {
