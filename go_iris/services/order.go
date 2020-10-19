873908615340cb774e003bd8956cecc3d40a3ed6
caoting <307298900@qq.com>
2020-10-20 00:13:24 +0800

    order insert by msg

--- go_iris/services/order.go
+++ go_iris/services/order.go
@@ -12,34 +12,44 @@ type IOrderService interface {
 	Insert(order *datamodels.Order) (int64, error)
 	GetAll() ([]*datamodels.Order, error)
 	GetAllWithInfo() (map[int]map[string]string, error)
+	InsertByMsg(message *datamodels.Message) (int64, error)
 }
 
 type OrderService struct {
 	repository repositories.IOrder
 }
 
-func (o OrderService) GetById(id int64) (*datamodels.Order, error) {
+func (o *OrderService) GetById(id int64) (*datamodels.Order, error) {
 	return o.repository.SelectByKey(id)
 }
 
-func (o OrderService) DeleteById(id int64) bool {
+func (o *OrderService) DeleteById(id int64) bool {
 	return o.repository.Delete(id)
 }
 
-func (o OrderService) Update(order *datamodels.Order) error {
+func (o *OrderService) Update(order *datamodels.Order) error {
 	return o.repository.Update(order)
 }
 
-func (o OrderService) Insert(order *datamodels.Order) (int64, error) {
+func (o *OrderService) Insert(order *datamodels.Order) (int64, error) {
 	return o.repository.Insert(order)
 }
 
-func (o OrderService) GetAll() ([]*datamodels.Order, error) {
+func (o *OrderService) GetAll() ([]*datamodels.Order, error) {
 	return o.repository.SelectAll()
 }
 
-func (o OrderService) GetAllWithInfo() (map[int]map[string]string, error) {
+func (o *OrderService) GetAllWithInfo() (map[int]map[string]string, error) {
 	return o.repository.SelectAllWithInfo()
+}
+
+func (o *OrderService) InsertByMsg(message *datamodels.Message) (int64, error) {
+	order := &datamodels.Order{
+		UserId:      message.UserId,
+		ProductId:   message.ProductId,
+		OrderStatus: datamodels.OrderSuccess,
+	}
+	return o.Insert(order)
 }
 
 func NewService(r repositories.IOrder) IOrderService {
