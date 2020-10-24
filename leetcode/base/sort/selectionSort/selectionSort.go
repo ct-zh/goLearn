1ce6b88519a6a11bf64a8b4df990f9a4645efeaf
caoting <307298900@qq.com>
2020-10-24 13:49:22 +0800

    helper

--- leetcode/base/sort/selectionSort/selectionSort.go
+++ leetcode/base/sort/selectionSort/selectionSort.go
@@ -2,9 +2,9 @@ package selectionSort
 
 import "fmt"
 
-// 选择排序
+// 选择排序 时间复杂度 O(n^2)
 type SelectionSort struct {
-	Arr     map[int]interface{}       // 需要排序的数组
+	Arr     map[int]int               // 需要排序的数组
 	N       int                       // 数组元素总数
 	Compare func(k1 int, k2 int) bool // 比较函数
 }
@@ -12,19 +12,17 @@ type SelectionSort struct {
 // 进行选择排序
 func (s *SelectionSort) Do() {
 	for index := 0; index < s.N; index++ {
-		minIndex := index	// 注意这里的minIndex从index开始递增,而不是从0开始递增
+		minIndex := index // 注意这里的minIndex从index开始递增,而不是从0开始递增
 
 		for i := index + 1; i < s.N; i++ {
 			// 寻找 [i, n) 区间里的最小值
-			if s.Compare(minIndex, i) {
+			if s.Arr[i] < s.Arr[minIndex] {
 				minIndex = i
 			}
 		}
 
-		// swap
-		mid := s.Arr[index]
-		s.Arr[index] = s.Arr[minIndex]
-		s.Arr[minIndex] = mid
+		// swap 交换两个值
+		s.Arr[index], s.Arr[minIndex] = s.Arr[minIndex], s.Arr[index]
 	}
 }
 
@@ -35,6 +33,3 @@ func (s *SelectionSort) P() {
 	}
 	fmt.Println("============")
 }
-
-
-
