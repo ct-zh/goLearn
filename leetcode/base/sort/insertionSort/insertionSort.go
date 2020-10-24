73f7d0b47e19f9290059d1fc1244742072cad1ce
caoting <307298900@qq.com>
2020-10-24 14:21:57 +0800

    insertion

--- leetcode/base/sort/insertionSort/insertionSort.go
+++ leetcode/base/sort/insertionSort/insertionSort.go
@@ -21,7 +21,7 @@ func (i *InsertionSort) Do() {
 		// 每次与前面一个数做比较，因为最后是与arr[0]做比较，所以这里 n > 0就可以了
 		for n := m; n > 0; n-- {
 			if i.Compare(i.Arr[n], i.Arr[n-1]) { // 每次与前一个元素比较
-				i.swap(n, n-1)
+				i.Arr[n], i.Arr[n-1] = i.Arr[n-1], i.Arr[n]
 			} else {
 				break // 插入排序相对选择排序的优点：可能会提前结束
 			}
@@ -36,33 +36,9 @@ func (i *InsertionSort) Do() {
 	fmt.Println(i.Arr)
 }
 
-// 插入排序 优化版
-//
-func (i *InsertionSort) Do2() {
-	for m := 1; m < i.N; m++ {
-		save := i.Arr[m]
-		fmt.Printf("Save = %d\n", save)
-
-		for n := m; n > 0; n-- { // n: 1
-			// 如果不符合条件，说明 n 应该在 n-1 的前面
-			// 相对上面的版本，这里我们不交换两个数据，而是仅仅将 n - 1 赋给 n
-
-			if n > 0 && i.Compare(save, i.Arr[n-1]) {
-				i.Arr[n] = i.Arr[n-1]
-				fmt.Printf("比较：Save: %d 小于 Target(Arr[n-1]): %d 交换后：Arr[n]=%d  Arr[n-1]=%d \n",
-					save, i.Arr[n-1], i.Arr[n], i.Arr[n-1])
-			} else {
-				fmt.Printf("比较：Save: %d 大于 Target(Arr[n-1]): %d 交换： Arr[n]=%d \n", save, i.Arr[n-1], i.Arr[n])
-				i.Arr[n] = save
-			}
-		}
-	}
-
-	fmt.Printf("结果： %+v\n", i.Arr)
-}
-
+// 插入排序优化版，可以少做一次数据交换
 // 优化后的插排比选择排要快很多， 尤其在数组中重复数据较多的情况下
-func (i *InsertionSort) Do3() {
+func (i *InsertionSort) DoBetter() {
 	for m := 1; m < i.N; m++ {
 		elem := i.Arr[m]
 		var n int
@@ -71,15 +47,6 @@ func (i *InsertionSort) Do3() {
 		}
 		i.Arr[n] = elem
 	}
-}
-
-// 交换两个的值
-func (i *InsertionSort) swap(k1 int, k2 int) {
-	save := i.Arr[k1]
-	i.Arr[k1] = i.Arr[k2]
-	i.Arr[k2] = save
-
-	//fmt.Printf("交换： k1: %d k2: %d \n", k1, k2)
 }
 
 func (i *InsertionSort) P() {
