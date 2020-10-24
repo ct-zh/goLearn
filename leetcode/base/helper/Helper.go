3b4ee282a157f6d9877fbe7668b18e243d3ce119
caoting <307298900@qq.com>
2020-10-24 09:59:20 +0800

    helper

--- leetcode/base/helper/Helper.go
+++ leetcode/base/helper/Helper.go
@@ -6,14 +6,13 @@ import (
 	"time"
 )
 
-// 生成指定范围的整数数组
-func GenerateRandomArray(n int, rangeL int, rangeR int) map[int]interface{} {
+// 生成大小为n,元素在[rangeL,rangeR]区间的 整数数组
+func GenerateRandomArray(n int, rangeL int, rangeR int) map[int]int {
 	if rangeL > rangeR {
 		return nil
 	}
 
-	// rand
-	arr := map[int]interface{}{}
+	arr := map[int]int{} // rand
 
 	rand2 := rand.New(rand.NewSource(time.Now().Unix()))
 	for i := 0; i < n; i++ {
@@ -24,10 +23,21 @@ func GenerateRandomArray(n int, rangeL int, rangeR int) map[int]interface{} {
 	return arr
 }
 
-func GenerateNearlyArray(n int, swapTimes int) map[int]interface{} {
-	var arr map[int]interface{}
+// 生成近似顺序的整数数组
+// n: 数组大小; swapTimes: 数组打乱次数;
+func GenerateNearlyArray(n int, swapTimes int) map[int]int {
+	arr := make(map[int]int)
 	for i := 0; i < n; i++ {
 		arr[i] = i
 	}
+	rand2 := rand.New(rand.NewSource(time.Now().Unix()))
+	for i := 0; i < swapTimes; i++ {
+		// 取两个 [0,n) 区间的数
+		posX := rand2.Int() % n
+		posy := rand2.Int() % n
+		// swap 随机交换两个数据
+		arr[posX], arr[posy] = arr[posy], arr[posX]
+	}
 
+	return arr
 }
