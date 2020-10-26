a451cf44e024db7265ab618760d7f5681f261d0b
caoting <307298900@qq.com>
2020-10-26 19:49:45 +0800

    selection sort

--- leetcode/base/sort/selectionSort/selectionSort.go
+++ leetcode/base/sort/selectionSort/selectionSort.go
@@ -1,21 +1,19 @@
 package selectionSort
 
-import "fmt"
-
 // 选择排序 时间复杂度 O(n^2)
+// 每次在子list中找出最小值，与子list的第一个值交换
 type SelectionSort struct {
-	Arr     map[int]int               // 需要排序的数组
-	N       int                       // 数组元素总数
-	Compare func(k1 int, k2 int) bool // 比较函数
+	Arr   map[int]int // 需要排序的数组
+	Count int         // 数组元素总数
 }
 
 // 进行选择排序
 func (s *SelectionSort) Do() {
-	for index := 0; index < s.N; index++ {
+	for index := 0; index < s.Count; index++ {
 		minIndex := index // 注意这里的minIndex从index开始递增,而不是从0开始递增
 
-		for i := index + 1; i < s.N; i++ {
-			// 寻找 [i, n) 区间里的最小值
+		for i := index + 1; i < s.Count; i++ {
+			// 寻找 [i, Count) 区间里的最小值
 			if s.Arr[i] < s.Arr[minIndex] {
 				minIndex = i
 			}
@@ -24,12 +22,4 @@ func (s *SelectionSort) Do() {
 		// swap 交换两个值
 		s.Arr[index], s.Arr[minIndex] = s.Arr[minIndex], s.Arr[index]
 	}
-}
-
-// 打印数组列表
-func (s *SelectionSort) P() {
-	for k, v := range s.Arr {
-		fmt.Printf("Key-Value: %d - %+v \n", k, v)
-	}
-	fmt.Println("============")
 }
--- leetcode/base/sort/selectionSort/selectionSort_test.go
+++ leetcode/base/sort/selectionSort/selectionSort_test.go
@@ -1,22 +1,42 @@
 package selectionSort
 
 import (
+	"fmt"
+	Hepler "github.com/LannisterAlwaysPaysHisDebts/goLearn/leetcode/base/helper"
 	"testing"
+	"time"
 )
-
-type Student struct {
-	name  string
-	score int
-}
-
-func (s *Student) less(other Student) bool {
-	return s.score < other.score
-}
 
 // 测试内容
 // 1. 随机生成测试用例
 // 2. 排序成功的判断(check 函数)
 // 3. 平均运行时间：(时间复杂度测试)
 func TestSelectionSort(t *testing.T) {
+	tests := []struct {
+		arr map[int]int
+	}{
+		{Hepler.GenerateRandomArray(100, 0, 999)},
+		{Hepler.GenerateRandomArray(10000, 0, 99999)},
+	}
+	for key, tt := range tests {
+		startTime := time.Now()
 
+		count := len(tt.arr)
+		s := SelectionSort{
+			Arr:   tt.arr,
+			Count: count,
+		}
+		s.Do()
+
+		// check
+		for i := 1; i < count; i++ {
+			if s.Arr[i-1] > s.Arr[i] {
+				t.Errorf("[%d] 排序算法有问题：\n 原始数据：   %+v \n  排序后的数据： %+v\n", key, tt.arr, s.Arr)
+			}
+		}
+
+		// 运行时间
+		duration := time.Now().Sub(startTime)
+		fmt.Printf("[%d] 消耗时间： %.8fs\n", key, duration.Seconds())
+	}
 }
