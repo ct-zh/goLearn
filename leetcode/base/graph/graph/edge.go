fc122fbcd14e616ef97b988155834e708af0494e
caoting <307298900@qq.com>
2020-11-05 14:22:46 +0800

    带权图

---
+++ leetcode/base/graph/graph/edge.go
@@ -0,0 +1,36 @@
+package graph
+
+type Weight interface {
+	// 比较两个权的大小
+	Compare(other Weight) int
+}
+
+// 有权图的边的内容
+type Edge struct {
+	a      int // 边的两个顶点a, b
+	b      int
+	weight Weight
+}
+
+func (e *Edge) V() int {
+	return e.a
+}
+
+func (e *Edge) W() int {
+	return e.b
+}
+
+func (e *Edge) Wt() Weight {
+	return e.weight
+}
+
+// 获取边里非节点x的节点
+func (e *Edge) Other(x int) int {
+	if e.a == x {
+		return e.b
+	} else if e.b == x {
+		return e.a
+	} else {
+		panic("x 必须是 两个节点中的一个")
+	}
+}
