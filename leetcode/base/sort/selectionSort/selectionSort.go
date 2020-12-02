ce9ce7ebe0f588f2b33f2ec76eaa8bc654fcd3af
caoting <307298900@qq.com>
2020-12-02 11:43:16 +0800

    选择排序公开

--- leetcode/base/sort/selectionSort/selectionSort.go
+++ leetcode/base/sort/selectionSort/selectionSort.go
@@ -2,7 +2,7 @@ package selectionSort
 
 // 选择排序 时间复杂度 O(n^2)
 // 每次在子list中找出最小值，与子list的第一个值交换
-func selectionSort(arr []int) {
+func SelectionSort(arr []int) {
 	l := len(arr)
 	for i := 0; i < l; i++ {
 		min := i                     // 当前子区间的第一个值i
