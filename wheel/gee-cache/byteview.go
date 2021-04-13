4440f3249b81b5f4a30b8df34434980d77688cb7
caoting <307298900@qq.com>
2021-04-14 00:49:32 +0800

    update: gee cache

---
+++ wheel/gee-cache/byteview.go
@@ -0,0 +1,24 @@
+package gee_cache
+
+// 存储真实的缓存值; 只读
+type ByteView struct {
+	b []byte
+}
+
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
