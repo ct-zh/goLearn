52d17311f69f797c7565a100d3c5900341696485
caoting <307298900@qq.com>
2023-03-28 19:19:52 +0800

    update

--- doc/net/http/demo_server/demo_for_weixin/main.go
+++ doc/net/http/demo_server/demo_for_weixin/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/schema"
 	"io/ioutil"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/schema"
+	"net/url"
+	"reflect"
+	"sync"
 )
+
+var decoder *ReqDecoder
 
 type weixinRequest struct {
 	Signature string `json:"signature"`
@@ -20,11 +24,12 @@ type weixinRequest struct {
 // nginx 80接口反向代理
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		wx := &weixinRequest{}
-		decoder := schema.NewDecoder()
-		err := decoder.Decode(wx, request.URL.Query())
+		// 解析url的query参数
+		log.Printf("query=%+v", request.URL.Query())
+		wx, err := decoder.Decode(request.URL.Query())
 		if err != nil {
-			log.Printf("err= %+v \n", err)
+			log.Printf("decoder.Decode err=%+v", err)
+			writer.Write([]byte(""))
 			return
 		}
 		log.Printf("wx=%+v", wx)
@@ -40,8 +45,44 @@ func main() {
 
 		writer.Write([]byte(wx.Echostr))
 	})
+	decoder = NewReqDecoder()
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
+}
+
+type ReqDecoder struct {
+	Decoder     *schema.Decoder
+	FilterQuery sync.Map
+}
+
+func NewReqDecoder() *ReqDecoder {
+	t := reflect.TypeOf(weixinRequest{})
+	filter := sync.Map{}
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		filter.Store(field.Tag.Get("json"), struct{}{})
+	}
+	return &ReqDecoder{
+		Decoder:     schema.NewDecoder(),
+		FilterQuery: filter,
+	}
+}
+
+func (r *ReqDecoder) Decode(val url.Values) (*weixinRequest, error) {
+	filterQuery := make(map[string][]string)
+	for key, strings := range val {
+		if _, ok := r.FilterQuery.Load(key); ok {
+			filterItem := strings
+			filterQuery[key] = filterItem
+		}
+	}
+	log.Printf("filter query=%+v", filterQuery)
+	wx := &weixinRequest{}
+	err := r.Decoder.Decode(wx, filterQuery)
+	if err != nil {
+		return nil, err
+	}
+	return wx, nil
 }
