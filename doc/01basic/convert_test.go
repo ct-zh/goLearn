34350bd196854c57d189480b539f53c705468ff7
caoting <307298900@qq.com>
2023-04-03 16:03:12 +0800

    update: 不要再使用fmt.Sprintf来转换类型了

---
+++ doc/01basic/convert_test.go
@@ -0,0 +1,23 @@
+package _1basic
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+// int64转换成string的两种方法，基准测试
+// 1.strconv.FormatInt
+// 2.Sprintf
+func BenchmarkInt64ConvertToString1_FormatInt(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var ii int64 = 10
+		_ = strconv.FormatInt(ii, 10)
+	}
+}
+func BenchmarkInt64ConvertToString2_Sprintf(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var ii int64 = 10
+		_ = fmt.Sprintf("%d", ii)
+	}
+}
